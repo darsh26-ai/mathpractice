a056bf7b36f416d55827e6c1e1d370a83c2a9b73
darsh26-ai <ursdarshan26@gmail.com>
2026-08-08 16:17:01 -0400

    Create grades object with subjects for each grade
    Added a grades object containing subjects for each grade level.

---
+++ js/data.js
@@ -0,0 +1,9 @@
+const grades = {
+    1: ["addition", "subtraction", "wordProblems"],
+    2: ["addition", "subtraction", "multiplication", "division", "fractions", "wordProblems"],
+    3: ["addition", "subtraction", "multiplication", "division", "fractions", "time", "wordProblems"],
+    4: ["addition", "subtraction", "multiplication", "division", "fractions", "decimals", "time", "wordProblems"],
+    5: ["addition", "subtraction", "multiplication", "division", "fractions", "decimals", "percent", "wordProblems"],
+    6: ["integers", "fractions", "decimals", "ratio", "percent", "time", "wordProblems"],
+    7: ["integers", "rational", "algebra", "exponents", "time", "wordProblems"]
+};
